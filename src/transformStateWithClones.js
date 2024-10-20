d0a7af4c81addf77b390b8c8f65c31164de84fa3
nex1994 <bilicki1994@gmail.com>
2024-10-20 19:04:45 +0200

    init commit

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
-}
+  const stateHistory = [];
+  const newState = { ...state };
 
+  for (const keys in actions) {
+    if (actions[keys].type === 'addProperties') {
+      Object.assign(newState, actions[keys].extraData);
+    }
+
+    if (actions[keys].type === 'removeProperties') {
+      const properties = actions[keys].keysToRemove;
+
+      for (let i = 0; i < properties.length; i++) {
+        delete newState[properties[i]];
+      }
+    }
+
+    if (actions[keys].type === 'clear') {
+      for (const key in newState) {
+        delete newState[key];
+      }
+    }
+
+    const updatedState = { ...newState };
+
+    stateHistory.push(updatedState);
+  }
+
+  return stateHistory;
+}
 module.exports = transformStateWithClones;
